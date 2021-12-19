2541c163d3f399ffac71c7b233d519ab15782fcb
victor <victor.eduardo.sntos@gmail.com>
2021-12-18 22:55:36 -0300

    Initial commit

---
+++ src/test/java/com/ead/msserviceregistry/MsServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ead.msserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
